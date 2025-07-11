0529ef81a0905575414fc3e9cc64c0f72897464a
papalorixoyrolex <testisquitcc@gmail.com>
2025-07-11 22:47:47 +0300

    make new logo and also try offline mode

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,26 +1,27 @@
-const CACHE_NAME = 'emotions-store-v1.6';
+const CACHE_NAME = 'emotions-store-v1.61';
 const ASSETS_TO_CACHE = [
   '/',
   '/index.html',
   '/manifest.json',
-  '/serviceWorker.js',
   '/header_styles.css',
   '/favicon.png',
   '/habits.html',
   '/history.html',
   '/task.html',
+  '/swipe-nan.js'
 ];
 
 self.addEventListener('install', e => {
   e.waitUntil(
     caches.open(CACHE_NAME).then(cache =>
       Promise.all(ASSETS_TO_CACHE.map(url =>
-        fetch(url, { cache: 'reload' })
+        fetch(url)
           .then(response => {
             if (!response.ok) throw new Error(`Не вдалося завантажити ${url}`);
             return cache.put(url, response);
           })
       ))
+
     )
   );
   self.skipWaiting();
@@ -40,6 +41,6 @@ self.addEventListener('fetch', e => {
   e.respondWith(
     caches.match(e.request)
       .then(response => response || fetch(e.request))
-      .catch(() => new Response('', { status: 200, statusText: 'Offline fallback' }))
+      .catch(() => caches.match('/index.html'))
   );
 });
